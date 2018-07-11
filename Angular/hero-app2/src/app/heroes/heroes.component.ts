c5f52ed54a0bc67954da8e50b313e77d3175ffde
martinfreefall <martinfreefall@gmail.com>
2018-07-12 02:08:06 +0530

    Routing Done - Angular.

--- Angular/hero-app2/src/app/heroes/heroes.component.ts
+++ Angular/hero-app2/src/app/heroes/heroes.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes';
 
 import {HeroService} from '../hero.service';
-import {throwError} from 'rxjs';
 
 @Component({
   selector: 'app-heroes',
@@ -13,11 +11,6 @@ import {throwError} from 'rxjs';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
-  selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
