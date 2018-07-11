009bee4fe94be293626c58f3a08436d400d55911
martinfreefall <martinfreefall@gmail.com>
2018-07-11 08:47:04 +0530

    first commit - upto services

---
+++ Angular/hero-app2/src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Mr noce guy'},
+  {id: 12, name: 'Mr noce guy'},
+  {id: 13, name: 'Mr noce guy'},
+  {id: 14, name: 'Mr noce guy'},
+  {id: 15, name: 'Mr noce guy'},
+  {id: 16, name: 'Mr noce guy'},
+  {id: 17, name: 'Mr noce guy'},
+  {id: 18, name: 'Mr noce guy'},
+  {id: 19, name: 'Mr noce guy'},
+];
